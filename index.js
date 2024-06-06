ea9cf41cb4289f9055d80aefd998a247dae11a6e
zhiwen.huang <zhiwen.huang@zealcomm.cn>
2024-06-06 19:20:25 +0800

    add axios

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { init: initDB, Counter } = require("./db");
+const axios = require('axios');
 
 const logger = morgan("tiny");
 
